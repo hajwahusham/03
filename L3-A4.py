b9fe0ffd99bda1603a3c38a4810a93b76e8d047a
hajwahusham <hajwahusham@gmail.com>
2025-07-28 17:48:53 +0500

    activity04

---
+++ L3-A4.py
@@ -0,0 +1,10 @@
+import datetime, calendar
+
+# using now() to get current time
+current_time = datetime.datetime.today()
+
+# printing the value of current_time
+print("time now at Greenwich Meridian is: ", current_time)
+
+# printing the calendar
+print("\n", calendar.calendar(1945))
