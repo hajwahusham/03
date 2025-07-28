0f88a7d1cadf4f6f2219ee5e588b6cd2997a3c22
hajwahusham <hajwahusham@gmail.com>
2025-07-28 17:13:46 +0500

    Acti vity

---
+++ L3-A1.py
@@ -0,0 +1,8 @@
+number = int(input("enter a number"))
+print("number to be checked:", number)
+
+if number%2==0:
+    print("this is an even number")
+    
+else:
+    print("this is an odd number")
